8f9da9ec3692403141cd6e7854dfb731306c0213
Andrea Stagi <stagi.andrea@gmail.com>
2015-05-13 19:33:02 +0200

    Use Gruntfile instead of Makefile

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        qunit: {
+            all: ['tests/tests.html']
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.license %> */\n'
+            },
+            build: {
+                files: {
+                    'build/cookiebanner-<%= pkg.version %>.min.js': 'src/cookiebanner.js'
+                }
+            }
+        },
+        jshint: {
+            files: ["src/cookiebanner.js"],
+        },
+
+    });
+
+    for (var key in grunt.file.readJSON('package.json').devDependencies) {
+        if (key !== 'grunt' && key.indexOf('grunt') === 0) {
+            grunt.loadNpmTasks(key);
+        }
+    }
+
+    grunt.registerTask('default', ['jshint', 'qunit', 'uglify']);
+    grunt.registerTask('ci', ['jshint', 'qunit']);
+};
